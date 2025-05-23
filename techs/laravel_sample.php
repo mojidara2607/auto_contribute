96dea02ed07fd2efd903f9f0d8d2f32ebabdbe0f
Mojidara Vivek <vivekprajapati044@gmail.com>
2025-05-23 11:25:16 +0530

    Update logic slightly

--- techs/laravel_sample.php
+++ techs/laravel_sample.php
@@ -3,3 +3,4 @@
 // Auto commit for Laravel at 2025-05-19 12:39:50
 // Auto commit for Laravel at 2025-05-21 15:16:23
 // Auto commit for Laravel at 2025-05-22 12:47:57
+// commit at 2025-05-23 11:25:16
